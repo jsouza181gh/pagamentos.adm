98474e28f585c3d3ceb7c06237f23ed6566b3409
jsouza181gh <emanuelrodrigues010@gmail.com>
2025-10-31 11:27:56 -0300

    Adicionados métodos POST nas páginas de Cadastro e Login

--- routes.py
+++ routes.py
@@ -1,17 +1,21 @@
 from main import app
 from crud import buscarFornecedor
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request, Blueprint
 
 @app.route('/')
 def homepage():
     return redirect(url_for('login'))
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template('login.html')
 
-@app.route('/cadastro')
+@app.route('/cadastro', methods=['GET', 'POST'])
 def cadastro():
+    data = request.form
+    print(data)
     return render_template('cadastro.html')
 
 @app.route('/api/<idFornecedor>')
