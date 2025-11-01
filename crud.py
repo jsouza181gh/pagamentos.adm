bbf096cefe53edb697df67c090932b5862cb9445
jsouza181gh <emanuelrodrigues010@gmail.com>
2025-11-01 13:31:22 -0300

    Funções comentadas

--- crud.py
+++ crud.py
@@ -57,4 +57,4 @@ def exibirFornecedores():
 #criarFornecedor('João Emanuel Souza Rodrigues', '02.336.124/0014-92', '21979405435', 'joao.rodrigues@global.komatsu', 'qwertyje123')
 #buscarFornecedor(1)
 #editarFornecedor(2,'João Emanuel Souza Rodrigues', '02.336.124/0014-92', 'joao.rodrigues@global.komatsu', 'qwertyje123')
-excluirFornecedor(1)
+#excluirFornecedor(1)
